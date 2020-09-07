ac760184ce508d77e838cd46497cacd159fdb783
forkanju <forkanju44rth@gmail.com>
2020-09-06 17:41:55 -0700

    flutter animated splash screen project commit

---
+++ android/app/src/main/java/com/forkan/data_passing/MainActivity.java
@@ -0,0 +1,6 @@
+package com.forkan.data_passing;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'dart:async';
+
+import 'package:data_passing/home.dart';
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Splash Animation'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    Timer(Duration(seconds: 5), (){
+      Navigator.push(context, CupertinoPageRoute(builder: (context) => HomePage()));
+    });
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        child: FlareActor("animations/Resizing House.flr",
+        alignment: Alignment.center,
+        fit: BoxFit.cover,
+        animation: "Demo Mode",),
+      ),
+    );
+  }
+}
